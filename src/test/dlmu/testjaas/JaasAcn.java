0ac61eaeff40d85972b978ce82b19175ef5f4227
heipacker <heipackermail@gmail.com>
2013-09-01 11:05:13 +0800

    first commit

---
+++ src/test/dlmu/testjaas/JaasAcn.java
@@ -0,0 +1,39 @@
+package test.dlmu.testjaas;
+
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import com.sun.security.auth.callback.TextCallbackHandler;
+
+public class JaasAcn {
+
+	public static void main(String[] args) {
+		// Obtain a LoginContext, needed for authentication. Tell
+		// it to use the LoginModule implementation specified by
+		// the entry named "JaasSample" in the JAAS login
+		// configuration file and to also use the specified
+		// CallbackHandler.
+		LoginContext lc = null;
+		try {
+			lc = new LoginContext("JaasSample", new TextCallbackHandler());
+		} catch (LoginException le) {
+			System.err.println("Cannot create LoginContext. " + le.getMessage());
+			System.exit(-1);
+		} catch (SecurityException se) {
+			System.err.println("Cannot create LoginContext. " + se.getMessage());
+			System.exit(-1);
+		}
+
+		try {
+			// attempt authentication
+			lc.login();
+
+		} catch (LoginException le) {
+			System.err.println("Authentication failed: ");
+			System.err.println("  " + le.getMessage());
+			System.exit(-1);
+		}
+
+		System.out.println("Authentication succeeded!");
+	}
+}
