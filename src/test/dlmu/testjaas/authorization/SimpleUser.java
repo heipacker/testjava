91004c167ef23222eebb43b60856aae7a4a08a56
heipacker <heipackermail@gmail.com>
2013-09-05 20:55:01 +0800

    first commit

---
+++ src/test/dlmu/testjaas/authorization/SimpleUser.java
@@ -0,0 +1,17 @@
+package test.dlmu.testjaas.authorization;
+/**
+ * 简单对象
+ * @author Administrator
+ *
+ */
+public class SimpleUser {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
