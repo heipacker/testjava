70485ba4241ad22afe05b47ef161709850058111
heipacker <heipackermail@gmail.com>
2013-09-07 21:59:57 +0800

    first commit

---
+++ src/test/dlmu/testenum/TestCard.java
@@ -0,0 +1,24 @@
+package test.dlmu.testenum;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestCard {
+	public static void main(String args[]) {
+		int numHands = Integer.parseInt(args[0]);
+		int cardsPerHand = Integer.parseInt(args[1]);
+		List<Card> deck = Card.newDeck();
+		Collections.shuffle(deck);
+		for (int i = 0; i < numHands; i++)
+			System.out.println(deal(deck, cardsPerHand));
+	}
+
+	public static ArrayList<Card> deal(List<Card> deck, int n) {
+		int deckSize = deck.size();
+		List<Card> handView = deck.subList(deckSize - n, deckSize);
+		ArrayList<Card> hand = new ArrayList<Card>(handView);
+		handView.clear();
+		return hand;
+	}
+}
