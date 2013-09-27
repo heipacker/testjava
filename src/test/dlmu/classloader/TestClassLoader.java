6750bf0d328381718ed23d46dcb95f6cee49593c
heipacker <heipackermail@gmail.com>
2013-09-27 21:52:36 +0800

    first commit！

---
+++ src/test/dlmu/classloader/TestClassLoader.java
@@ -0,0 +1,17 @@
+package test.dlmu.classloader;
+
+public class TestClassLoader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+		System.out.println("系统类装载器:" + systemClassLoader);
+		ClassLoader extClassLoader = systemClassLoader.getParent();
+		System.out.println("系统类装载器的父类加载器——扩展类加载器:" + extClassLoader);
+		ClassLoader bootClassLoader = extClassLoader.getParent();
+		System.out.println("扩展类加载器的父类加载器——引导类加载器:" + bootClassLoader);
+	}
+
+}
