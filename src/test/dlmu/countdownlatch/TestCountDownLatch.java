c1173ff860ae3b1e20ded6b8980b3a39af0d0769
heipacker <heipackermail@gmail.com>
2013-09-28 23:44:39 +0800

    commit first!

---
+++ src/test/dlmu/countdownlatch/TestCountDownLatch.java
@@ -0,0 +1,63 @@
+package test.dlmu.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestCountDownLatch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			long time = timeTasks(10, new Thread(){
+				@Override
+				public void run(){
+					long g = 0;
+					for(int i = 0; i < 1000; ++i){
+						g += i;
+					}
+					System.out.println(g);
+				}
+			});
+			
+			System.out.println("totalTime:" + time);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static long timeTasks(int nThreads, final Runnable task) throws InterruptedException{
+		final CountDownLatch startGate = new CountDownLatch(1);
+		final CountDownLatch endGate = new CountDownLatch(nThreads);
+		
+		for(int i = 0; i < nThreads; ++i){
+			Thread t = new Thread(){
+				@Override
+				public void run(){
+					try{
+						startGate.await();
+						try{
+							task.run();
+						}finally{
+							endGate.countDown();
+						}
+					} catch(InterruptedException ignored){
+						
+					}
+				}
+			};
+			t.start();
+		}
+		
+		long start = System.nanoTime();
+		startGate.countDown();
+		
+		endGate.await();
+		
+		long end = System.nanoTime();
+		
+		return end - start;
+	}
+}
