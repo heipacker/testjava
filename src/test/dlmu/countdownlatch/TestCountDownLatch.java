55338930625455041ea8d65599d3a2d0074f0cbb
heipacker <heipackermail@gmail.com>
2013-09-29 00:01:36 +0800

    222

--- src/test/dlmu/countdownlatch/TestCountDownLatch.java
+++ src/test/dlmu/countdownlatch/TestCountDownLatch.java
@@ -2,6 +2,11 @@ package test.dlmu.countdownlatch;
 
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * 测试闭锁
+ * @author Administrator
+ *
+ */
 public class TestCountDownLatch {
 
 	/**
@@ -10,54 +15,53 @@ public class TestCountDownLatch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			long time = timeTasks(10, new Thread(){
+			long time = timeTasks(10, new Thread() {
 				@Override
-				public void run(){
+				public void run() {
 					long g = 0;
-					for(int i = 0; i < 1000; ++i){
+					for (int i = 0; i < 1000; ++i) {
 						g += i;
 					}
 					System.out.println(g);
 				}
 			});
-			
+
 			System.out.println("totalTime:" + time);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public static long timeTasks(int nThreads, final Runnable task) throws InterruptedException{
+
+	public static long timeTasks(int nThreads, final Runnable task)
+			throws InterruptedException {
 		final CountDownLatch startGate = new CountDownLatch(1);
 		final CountDownLatch endGate = new CountDownLatch(nThreads);
-		
-		for(int i = 0; i < nThreads; ++i){
-			Thread t = new Thread(){
+
+		for (int i = 0; i < nThreads; ++i) {
+			Thread t = new Thread() {
 				@Override
-				public void run(){
-					try{
+				public void run() {
+					try {
 						startGate.await();
-						try{
+						try {
 							task.run();
-						}finally{
+						} finally {
 							endGate.countDown();
 						}
-					} catch(InterruptedException ignored){
-						
+					} catch (InterruptedException ignored) {
+
 					}
 				}
 			};
 			t.start();
 		}
-		
+
 		long start = System.nanoTime();
+		// 触发所有线程开始执行
 		startGate.countDown();
-		
+		// 等待所有线程执行结束
 		endGate.await();
-		
-		long end = System.nanoTime();
-		
-		return end - start;
+		return System.nanoTime() - start;
 	}
 }
