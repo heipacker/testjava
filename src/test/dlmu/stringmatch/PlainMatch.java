0ac5ba6f36cfe3e39dd0a5782063b463a539a7d3
heipacker <heipackermail@gmail.com>
2013-10-10 21:40:56 +0800

    222

--- src/test/dlmu/stringmatch/PlainMatch.java
+++ src/test/dlmu/stringmatch/PlainMatch.java
@@ -12,12 +12,12 @@ public class PlainMatch {
 		PlainMatch.match(target, pattern);
 	}
 	/**
-	 * 朴素匹配算法
+	 * 朴素匹配算法O((n-m+1)*m)
 	 * @param target
 	 * @param pattern
 	 */
 	private static void match(char[] target, char[] pattern){
-		for(int i = 0; i<target.length; ++i){
+		for(int i = 0; i<target.length-pattern.length; ++i){
 			int tmpIndex = i;
 			for(int j = 0; j<pattern.length; ++j){
 				if(target[tmpIndex++]!=pattern[j]){
@@ -30,7 +30,7 @@ public class PlainMatch {
 		}
 		System.out.println("////////////////////////////////////////////////////////////");
 		int i = 0;
-		while(i<target.length){
+		while(i<target.length-pattern.length){
 			int j = 0;
 			int tmpIndex = i++;
 			while(j<pattern.length){
