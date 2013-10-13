033f9873893ea08a8f20c5b433bdf30eee8e3eac
heipacker <heipackermail@gmail.com>
2013-10-13 12:37:49 +0800

    11

--- src/test/dlmu/file/lock/Test.java
+++ src/test/dlmu/file/lock/Test.java
@@ -6,10 +6,17 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Thread_writeFile thf3=new Thread_writeFile();    
-        Thread_readFile thf4=new Thread_readFile();    
-        thf3.start();    
+		Thread_writeFile thf3 = new Thread_writeFile();
+        Thread_readFile thf4 = new Thread_readFile();
+        thf3.start();
         thf4.start();
+        try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        System.exit(0);
 	}
 
 }
