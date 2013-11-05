d239ad6f4a0b1759a6acdd6e5a0713ef5d141cfd
heipacker <heipackermail@gmail.com>
2013-11-05 14:03:54 +0800

    guava

---
+++ src/test/dlmu/guava/Test.java
@@ -0,0 +1,15 @@
+package test.dlmu.guava;
+
+import com.google.common.base.Preconditions;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Preconditions.checkState(1>0);
+	}
+
+}
