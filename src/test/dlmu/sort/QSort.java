9a70a0904f60ec5460181f3fefc7a4c8c24299b7
heipacker <heipackermail@gmail.com>
2013-12-01 21:14:38 +0800

    change coding

--- src/test/dlmu/sort/QSort.java
+++ src/test/dlmu/sort/QSort.java
@@ -4,18 +4,18 @@ package test.dlmu.sort;
  *
  * @param <T>
  */
-public class QSort<T extends Comparable<T>> {
+public class QSort {
 
 	public static void main(String[] args){
 		Long[] array = new Long[]{(long) 9,(long)2,(long)7,(long) 12,(long)54,(long) 21,(long) 30,(long)39,(long)24,(long)14,(long) 56,(long)4,(long) 5};
-		QSort<Long> sort = new QSort<Long>();
-		sort.qsort(array, 0, array.length-1);
+		
+		qsort(array, 0, array.length-1);
 		for(Long t:array){
 			System.out.println(t);
 		}
 	}
 	
-	public void qsort(T[] array, int low, int high){
+	public static <T extends Comparable<T>> void qsort(T[] array, int low, int high){
 		if(low<high){
 			int middle = partition(array, low, high);
 			qsort(array, low, middle-1);
@@ -23,7 +23,7 @@ public class QSort<T extends Comparable<T>> {
 		}
 	}
 	
-	int partition(T[] array, int low, int high){
+	private static <T extends Comparable<T>> int partition(T[] array, int low, int high){
 		T middle = array[low];
 		while(low<high){
 			while(low<high && middle.compareTo(array[high])<0)	--high;
